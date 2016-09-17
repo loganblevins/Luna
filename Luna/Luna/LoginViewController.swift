c291081f1ec94890a8f6810d4d91aba4dd47cdcc
Logan Blevins <logan.blevins@covenanteyes.com>
2016-09-16 22:37:10 -0400

    Changed Swift compiler to treat warnings as errors.

---
+++ Luna/Luna/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Luna
+//
+//  Created by Logan Blevins on 9/13/16.
+//  Copyright © 2016 Logan Blevins. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController
+{
+
+}
