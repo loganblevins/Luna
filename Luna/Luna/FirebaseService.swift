91fc9ac1865b68cdab426b03dfb34c74bb76c6bd
wilcoxec <erika.wilcox@uky.edu>
2016-09-23 14:42:29 -0400

    Added User model
    added user model and view model

---
+++ Luna/Luna/FirebaseService.swift
@@ -0,0 +1,32 @@
+//
+//  FirebaseService.swift
+//  Luna
+//
+//  Created by Erika Wilcox on 9/23/16.
+//  Copyright © 2016 Logan Blevins. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+let URL_BASE = "https://luna-c2c2f.firebaseio.com"
+
+let FIREBASE_REF = FIRDatabase.database().reference()
+
+let REF_CURRENT_USER = FIRAuth.auth()?.currentUser
+
+
+class FirebaseService {
+    
+    var refCurrentUser = REF_CURRENT_USER?.uid
+    
+    var refUsers = FIREBASE_REF.child("users")
+    
+    var refDailyEntries = FIREBASE_REF.child("dailyEntries")
+    
+    var refEntry = FIREBASE_REF.child("entry")
+    
+}
+
+
