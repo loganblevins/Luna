e095146d3a4acf1cff4c9daa1ec6acc29ab83dde
Logan Blevins <logan.blevins@covenanteyes.com>
2016-09-23 12:20:02 -0400

    Stubbed Standard Defaults singleton.

---
+++ Luna/Luna/StandardDefaults.swift
@@ -0,0 +1,55 @@
+//
+//  StandardDefaults.swift
+//  Luna
+//
+//  Created by Logan Blevins on 9/23/16.
+//  Copyright © 2016 Logan Blevins. All rights reserved.
+//
+
+import Foundation
+
+protocol StandardDefaultsProtocol: class
+{
+	// Insert any members needed stored to disk.
+	// Use as getters and setters.
+	//
+}
+
+class StandardDefaults: StandardDefaultsProtocol
+{
+	// MARK: Public API
+	//
+	
+	// Everytime a member is updated it will automatically get stored to disk.
+	//
+	
+	// Insert any members needing stored to disk.
+	// Database files?
+	//
+
+	// MARK: Singleton Implementation
+	//
+	
+	static let sharedInstance = StandardDefaults()
+	private init() {}
+	
+	// MARK: Implementation Details
+	//
+	
+	// Insert keys for objects here
+	//
+	
+	private let standardDefaults = UserDefaults.standard
+	
+	private func getValue< T >( key: String ) -> T?
+	{
+		guard let objectValue = standardDefaults.object( forKey: key ) else
+		{
+			return nil
+		}
+		
+		return objectValue as? T
+	}
+	
+}
+
