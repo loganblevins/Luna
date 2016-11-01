d72b1b0c830bccc2b117ae31d5703769a7e54000
Daniel Ng <mdng223@g.uky.edu>
2016-11-01 11:48:12 -0400

    Made some test cases
    Wrote some test cases

--- Luna/LunaTests/LunaTests.swift
+++ Luna/LunaTests/LunaTests.swift
@@ -11,91 +11,132 @@ import XCTest
 
 class LunaAPITests: XCTestCase
 {
-	override func setUp()
-	{
-		super.setUp()
-		fakeRequestor = FakeRequestor()
-	}
+    
+    let authTokenLength = 40
+    let fireBaseTokenLength = 851
+    
+//	override func setUp()
+//	{
+//		super.setUp()
+//		fakeRequestor = FakeRequestor()
+//	}
+//	
+//	override func tearDown()
+//	{
+//		super.tearDown()
+//		fakeRequestor = nil
+//	}
+//	
+//	func testValidLogin()
+//	{
+//		let validCredentials = ( "someValidUsername", "someValidPassword" )
+//		
+//		fakeRequestor.response = Result.success( JSON.validLoginResponse() )
+//		
+//		let sut = LunaAPI( requestor: fakeRequestor )
+//		do
+//		{
+//			try sut.login( validCredentials )
+//			{
+//				result in
+//				
+//				// TODO: Handle result
+//			}
+//		}
+//		catch let error
+//		{
+//			// Safe to force down-cast as our `Network Error` since we're guaranteed to never encounter any other error type in this context.
+//			//
+//			let e = error as! NetworkError
+//			print( e.description )
+//			
+//			// TODO: Handle error
+//			//
+//		}
+//	}
 	
-	override func tearDown()
-	{
-		super.tearDown()
-		fakeRequestor = nil
+
+	func testValidObjectCount()
+	{       // expects an instance of auth token and firebase token
+        let jsonDict = JSON.validLoginResponse() as! Dictionary< String, Any >
+        XCTAssert( jsonDict.count == 2)
+        
 	}
-	
-	func testValidLogin()
-	{
-		let validCredentials = ( "someValidUsername", "someValidPassword" )
-		
-		fakeRequestor.response = Result.success( JSON.validLoginResponse() )
-		
-		let sut = LunaAPI( requestor: fakeRequestor )
-		do
-		{
-			try sut.login( validCredentials )
-			{
-				result in
-				
-				// TODO: Handle result
-			}
-		}
-		catch let error
-		{
-			// Safe to force down-cast as our `Network Error` since we're guaranteed to never encounter any other error type in this context.
-			//
-			let e = error as! NetworkError
-			print( e.description )
-			
-			// TODO: Handle error
-			//
-		}
-	}
-	
-	func testFailureWithTODO_SOME_ERRORLogin()
-	{
-		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
-		
-		fakeRequestor.response = Result.failure(<#T##Error?#>)
-		let sut = LunaAPI( requestor: fakeRequestor )
-
-	}
-	
-	func testFailureWithTODO_SOME_ERROR1Login()
-	{
-		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
-
-		
-		fakeRequestor.response = Result.failure(<#T##Error?#>)
-		let sut = LunaAPI( requestor: fakeRequestor )
-
-	}
-	
-	func testFailureWithTODO_SOME_ERROR2Login()
-	{
-		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
-		
-		fakeRequestor.response = Result.failure(<#T##Error?#>)
-		let sut = LunaAPI( requestor: fakeRequestor )
-
-	}
-	
-	func testFailureInvalidWithTODO_SOME_ERROR3Login()
-	{
-		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
-
-		fakeRequestor.response = Result.failure( NetworkError.invalid( "TODO: SOME ERROR MESSAGE OR NIL" ))
-		let sut = LunaAPI( requestor: fakeRequestor )
-
-	}
-	
-	func testFailureCannotParseWithTODO_SOME_ERROR4Login()
-	{
-		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
-
-		fakeRequestor.response = Result.failure( NetworkError.cannotParse( "TODO: SOME ERROR MESSAGE OR NIL" ) )
-		let sut = LunaAPI( requestor: fakeRequestor )
-
-	}
-	
-	fileprivate var fakeRequestor: FakeRequestor!
+    
+    // maybe make sure the objects are "auth_token" and "firebase_token"?
+    
+    func testValidObjectCount2()
+    {     // expects one instance of auth token
+        let jsonDict = JSON.validLoginResponse() as! Dictionary< String, Any >
+        let authToken = jsonDict["auth_token"] as! Dictionary< String, Any >
+        let count = authToken.count
+        XCTAssert( count == 1 )
+        
+    }
+   
+    func testValidObjectCount3()
+    {       // expects one instance of the token for auth token
+        let jsonDict = JSON.validLoginResponse() as! Dictionary< String, Any >
+        let authToken = jsonDict["auth_token"] as! Dictionary< String, Any >
+        let innerAuthToken = authToken["auth_token"] as! String
+        let count = innerAuthToken.count
+        XCTAssert( authTokenLength == count)
+    }
+    
+    func testValidObjectCount4()
+    {       // expects one instance of firebase token
+        let jsonDict = JSON.validLoginResponse() as! Dictionary< String, Any >
+        let firebaseToken = jsonDict["firebase_token"] as! Dictionary< String, Any >
+        let count = firebaseToken.count
+        XCTAssert( count == 1)
+    }
+    
+    func testValidObjectCount5()
+    {       // expects one instance of the token for firebasetoken
+        let jsonDict = JSON.validLoginResponse() as! Dictionary< String, Any >
+        let firebaseToken = jsonDict["firebase_token"] as! Dictionary< String, Any >
+        let innerAuthToken = firebaseToken["firebasetoken"] as! String
+        let count = innerAuthToken.count
+        XCTAssert( fireBaseTokenLength == count)
+    }
+    
+    //
+//	func testFailureWithTODO_SOME_ERROR1Login()
+//	{
+//		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
+//
+//		
+//		fakeRequestor.response = Result.failure(<#T##Error?#>)
+//		let sut = LunaAPI( requestor: fakeRequestor )
+//
+//	}
+//	
+//	func testFailureWithTODO_SOME_ERROR2Login()
+//	{
+//		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
+//		
+//		fakeRequestor.response = Result.failure(<#T##Error?#>)
+//		let sut = LunaAPI( requestor: fakeRequestor )
+//
+//	}
+//	
+//	func testFailureInvalidWithTODO_SOME_ERROR3Login()
+//	{
+//		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
+//
+//		fakeRequestor.response = Result.failure( NetworkError.invalid( "TODO: SOME ERROR MESSAGE OR NIL" ))
+//		let sut = LunaAPI( requestor: fakeRequestor )
+//
+//	}
+//	
+//	func testFailureCannotParseWithTODO_SOME_ERROR4Login()
+//	{
+//		let invalidCredentials = ( "someInvalidUsername", "someInvalidPassword" )
+//
+//		fakeRequestor.response = Result.failure( NetworkError.cannotParse( "TODO: SOME ERROR MESSAGE OR NIL" ) )
+//		let sut = LunaAPI( requestor: fakeRequestor )
+//
+//	}
+//	
+//	fileprivate var fakeRequestor: FakeRequestor!
 }
--- Luna/LunaTests/LunaTestsHelper.swift
+++ Luna/LunaTests/LunaTestsHelper.swift
@@ -11,33 +11,49 @@ import Foundation
 
 extension LunaAPITests
 {
-	class FakeRequestor: Requestor
-	{
-		var response: Result<Any>?
-
-		func request<T: Endpoint>( endpoint: T, credentials: Credentials?, completion: @escaping( _ result: Result<Any> ) -> Void )
-		{
-			// TODO: Implement handling of fake response.
-			//
-			completion( response! )
-			
-			
-		}
-		
-	}
+//	class FakeRequestor: Requestor
+//	{
+//		var response: Result<Any>?
+//
+//		func request<T: Endpoint>( endpoint: T, credentials: Credentials?, completion: @escaping( _ result: Result<Any> ) -> Void )
+//		{
+//			// TODO: Implement handling of fake response.
+//			//
+//			completion( response! )
+//			
+//			
+//		}
+//		
+//	}
 	
 	struct JSON
 	{
 		static func validLoginResponse() -> Any?
 		{
-			guard let _ = LunaAPITests.stringFromFile( path: "FakeLoginResponse" ) else { return nil }
-			// TODO: Manipulate JSON values/structure
-			//
-			
-			return nil
+            let jsonData = LunaAPITests.fetchFakeLoginResponseJsonData()
+            let json = try? JSONSerialization.jsonObject( with: jsonData!, options: .allowFragments )
+            return json
 		}
+        
+
+        // missing authentication token
+        static func invalidNoAuthenticationToken() -> Any?
+        {
+            let jsonData = LunaAPITests.fetchFakeLoginResponseJsonData()!
+            let json = try? JSONSerialization.jsonObject( with: jsonData, options: .allowFragments )
+            return json
+        }
+        
+        
 	}
 	
+    static func fetchFakeLoginResponseJsonData() -> Data?
+    {
+        guard let jsonString = LunaAPITests.stringFromFile( path: "FakeLoginResponse" ) else { return nil }
+        let jsonData = jsonString.data( using: .utf8 )
+        return jsonData
+    }
+    
 	static fileprivate func stringFromFile( path: String, type: String = "json" ) -> String?
 	{
 		guard let filePath = Bundle( for: self ).path( forResource: path, ofType: type ) else { return nil }
