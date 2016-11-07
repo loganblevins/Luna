9ea42ffd93492d6a4098748c5f34f3ba02d68932
Logan Blevins <logan.blevins@covenanteyes.com>
2016-11-07 14:16:04 -0500

    Added CVCalendar to pod file.

--- Luna/Pods/Target Support Files/Pods-Luna/Pods-Luna-frameworks.sh
+++ Luna/Pods/Target Support Files/Pods-Luna/Pods-Luna-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CVCalendar/CVCalendar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Freddy/Freddy.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CVCalendar/CVCalendar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Freddy/Freddy.framework"
 fi
